9c83267154da4b1b161952fc7047c217659d07c8
memreyagci <emre@meyagci.com>
2022-04-30 09:38:41 +0200

    Missing parameter added to constructor of Dog

--- taskSolutions/day39_Inheritance_Encapsulation/animal/Dog.java
+++ taskSolutions/day39_Inheritance_Encapsulation/animal/Dog.java
@@ -1,8 +1,8 @@
 package day39_Inheritance_Encapsulation.animal;
 
 public class Dog extends FriendlyAnimal {
-    public Dog(String name, String breed, String color, int age, double size) {
-        super(name, breed, color, age, size);
+    public Dog(String name, String breed, String color, int age, double size, char gender) {
+        super(name, breed, color, age, size, gender);
     }
 
     public void bark() {
